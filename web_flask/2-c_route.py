2381688e7cdb3a8754dc79f475bce4bf1e7f304a
Ronnie-Leon76 <ronleon76@gmail.com>
2023-10-24 15:31:12 +0300

    feat: C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -15,10 +15,12 @@ def hbnb():
     """Return HBNB"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
     """Return C followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
